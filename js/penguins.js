d0ef7a6eec56dc25037226336f3c244eca861129
Meowyam <github@pandashop.co.uk>
2013-02-05 17:53:10 +0000

    with scores and tries

--- js/penguins.js
+++ js/penguins.js
@@ -1,77 +1,92 @@
 function startgame() {
 
-var set1 = ["emperor1", "adelie1", "gentoo1", "snares1", "chinstrap1"];
-var set2 = ["emperor2", "adelie2", "gentoo2", "snares2", "chinstrap2"];
+    $("#tries").empty();
+    $("#score").empty();
 
-var clicked = false;
-var click1;
-var click2;
-var counter = 0;
+    var set1 = ["emperor1", "adelie1", "gentoo1", "snares1", "chinstrap1"];
+    var set2 = ["emperor2", "adelie2", "gentoo2", "snares2", "chinstrap2"];
 
-function loadcards() {
-    set1.shuffle();
-    set2.shuffle();
-    set1.deal('cardset1');
-    set2.deal('cardset2');
-}
+    var clicked = false;
+    var click1;
+    var click2;
+    var counter = 0;
+    var turns = 0;
 
-function matchcards() {
-loadcards();
-swapper();
-}
-
-Array.prototype.shuffle = function () {
-    for (var l = this.length, i, j; l>0; l--) {
-	var j = Math.floor(Math.random()*(l--));
-	i = this[l];
-	this[l] = this[j];
-	this[j] = i;
+    function loadcards() {
+	set1 = shuffle(set1);
+	set2 = shuffle(set2);
+	deal('cardset1',set1);
+	deal('cardset2',set2);
     }
-    return this;
-}
 
-Array.prototype.deal = function (cardset) {
-    $("#"+cardset).empty();
-    for (var i=0; i<this.length; i++) {
-    	$("#"+cardset).append("<img src=img/blank.png id=" + this[i] + " class=indv_penguin />");
+    function matchcards() {
+	loadcards();
+	swapper();
     }
-}
 
-function swapper() {
-    $(".indv_penguin").click(function() {
-    pimg = this.id.slice(0,-1); 
-    $("#"+this.id).attr("src", "img/"+pimg+".png");
-    matcher(this.id);
-    });
-}
+    function shuffle(setArray) {
+	for (var l = setArray.length, i, j; l>0; l--) {
+	    var j = Math.floor(Math.random()*(l--));
+	    i = setArray[l];
+	    setArray[l] = setArray[j];
+	    setArray[j] = i;
+	}
+	return setArray;
+    }
 
-function matcher(penguin) {
-    if (clicked == true) {
-	click2 = penguin;
-	console.log(click2);
-	if (click1.slice(0,-1) != click2.slice(0,-1)) {
-	    alert("sorry!");
-	    reset(click1, click2);
+    function deal(carddiv,cardset) {
+	$("#"+carddiv).empty();
+	for (var i=0; i<cardset.length; i++) {
+	    $("#"+carddiv).append("<img src=img/blank.png id=" + cardset[i] + " class=indv_penguin />");
+	}
+    }
+
+    function swapper() {
+	$(".indv_penguin").click(function() {
+		pimg = this.id.slice(0,-1); 
+		$("#"+this.id).attr("src", "img/"+pimg+".png");
+		matcher(this.id);
+		});
+    }
+
+    function matcher(penguin) {
+	if (clicked === true) {
+	    click2 = penguin;
+	    if (click1.slice(0,-1) != click2.slice(0,-1)) {
+	    	turns += 1;
+		comment(turns, "tries");
+		reset(click1, click2);
+	    }
+	    else {
+		counter += 1;
+		turns += 1;
+		comment(counter, "score");
+		comment(turns, "tries");
+		win(counter);
+	    }
+	    clicked=false;
 	}
 	else {
-	    counter += 1;
-	    alert(counter);
+	    clicked = true;
+	    click1 = penguin;
 	}
-	clicked=false;
     }
-    else {
-	clicked = true;
-	click1 = penguin;
-	console.log(click1.slice(0,-1));
-	console.log(clicked);
+
+    function reset(wrongone,wrongtwo) {
+
+	$("#"+wrongone).attr("src", "img/blank.png");
+	$("#"+wrongtwo).attr("src", "img/blank.png");
     }
-}
 
-function reset(wrongone,wrongtwo) {
-$("#"+wrongone).attr("src", "img/blank.png");
-$("#"+wrongtwo).attr("src", "img/blank.png");
-}
+    function win(counter) {
+    	if (counter === 5) { comment("All the penguins! Yay you win!", "score"); }
+	else return;
+    }
 
-matchcards();
+    function comment(comment,divname) {
+    	$("#"+divname).html(comment);
+    }
+
+    matchcards();
 
 }
